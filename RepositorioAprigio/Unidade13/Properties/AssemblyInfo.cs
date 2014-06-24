df9ee2542d1f1c73d2db1885337e42bb16ff64b4
AprigioJoseZancheta <prijotricolor@hotmail.com>
2014-06-24 01:10:03 -0300

    Commite Agora

--- RepositorioAprigio/Unidade13/Properties/AssemblyInfo.cs
+++ RepositorioAprigio/Unidade13/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Unidade3ExercicoFixacao")]
+[assembly: AssemblyTitle("Unidade13")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Unidade3ExercicoFixacao")]
+[assembly: AssemblyProduct("Unidade13")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d4d618b2-8a92-4111-a9b0-e9f3dc2035d6")]
+[assembly: Guid("cabfe1fc-402b-4fff-8f51-3c096fc3b6d7")]
 
 // Version information for an assembly consists of the following four values:
 //
